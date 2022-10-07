378e7165fa7887ca908ce2c76342a2d32f1dea5b
Josh Bamford <joshbamford09@gmail.com>
2022-10-07 22:10:15 +0100

    Completed all tests

--- src/strings.js
+++ src/strings.js
@@ -2,24 +2,24 @@ const sayHello = (string) =>{
   return 'Hello, ' + string + '!';
 };
 
-function uppercase (string) {
-  
+const uppercase = (string) => {
+  return string.toUpperCase();
 };
 
-function lowercase (string) {
-  // your code here
+const lowercase = (string) => {
+  return string.toLowerCase();
 };
 
-function countCharacters (string) {
-  // your code here
+const countCharacters = (string) =>{
+  return string.length
 };
 
-function firstCharacter (string) {
-  // your code here
+const firstCharacter = (string) =>{
+  return string.charAt(0);
 };
 
-function firstCharacters (string, n) {
-  // your code here
+const firstCharacters = (string, n) =>{
+ return string.slice(0, n);
 };
 
 module.exports = {
